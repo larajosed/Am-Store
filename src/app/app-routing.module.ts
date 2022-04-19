573ed2ee016f4d47fbbd8648284d46943e4969bb
Jose Daniel Lara <larajosed@gmail.com>
2022-04-19 19:13:42 +0200

    Navigation from carousel to product detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { ProductsListComponent } from './products-list/products-list.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
-  { path: 'products', component: ProductsListComponent },
   { path: 'product/:friendlyUrl', component: ProductDetailComponent }
 ];
 
